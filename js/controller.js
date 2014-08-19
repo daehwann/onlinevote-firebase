fb1996e4cdd1b446ebb82fb538553c577d665c6a
daehwan <daehwann@gmail.com>
2014-08-20 01:35:05 +0900

    fingerprintjs 적용

--- js/controller.js
+++ js/controller.js
@@ -13,23 +13,33 @@ app.controller('VoteViewController', ['$scope', 'FirebaseUtil',
 
 			return perc.toFixed(2);
 		}
+
+		$scope.key1 = function() {
+			var key1 = $scope.data.$keyAt(1);
+			alert(key1);
+		}
+
 	}	
 ])
 
 //vote page
-app.controller('VoteController', ['$scope', 'FirebaseUtil', 
+app.controller('VoteController', ['$scope', 'FirebaseUtil',
 	function($scope, FirebaseUtil) {
-		
-
 		$scope.vote = function(opinion) {
+			var fb = new Fingerprint();
+
 			var sync = FirebaseUtil.getSync();		
 			$scope.opinions = sync.$asArray();
 
-			$scope.opinions.$add( {
+			$scope.opinions.$add({
 				opinion : opinion,
-				comment : $scope.comment
+				regDate : new Date(),
+				comment : $scope.comment,
+				fingerprint : fb.get()
 			})
-			location.href='#/voteresult';
+			$scope.opinions.$destroy();
+
+			return 'Thank you for your vote!';
 		}
 	}
 ])
